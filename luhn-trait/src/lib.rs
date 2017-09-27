49c2dcea5e2623bb0da5d3276a05093574992f37
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-09-27 10:48:22 -0700

    Second iteration luhn-trait

--- luhn-trait/src/lib.rs
+++ luhn-trait/src/lib.rs
@@ -1,34 +1,15 @@
-pub trait Luhn { 
-    fn valid_luhn(&self) -> bool {
-        if let Some(digits) = self.digits() {
-            println!("{:?}", digits);
-            let mut total = 0;
-            for (i, d) in digits.iter().rev().enumerate() {
+pub trait Luhn { fn valid_luhn(&self) -> bool; }
+
+impl<T> Luhn for T where T: ToString { 
+    fn valid_luhn(&self) -> bool { 
+        let (mut total, mut i) = (0, 0);
+        for c in self.to_string().chars().rev() {
+            if let Some(d) = c.to_digit(10) { 
                 total += d * (((i as u32) % 2) + 1);
-                if d > &4u32 && i % 2 == 1 { total -= 9 }
-            }
-            digits.len() > 1 && total % 10 == 0
-        } else { false }
-    }
-
-    fn digits(&self) -> Option<Vec<u32>>;
-}
-
-
-impl Luhn for String { 
-    fn digits(&self) -> Option<Vec<u32>> { 
-        let mut digits: Vec<u32> = Vec::new();
-        for c in self.chars() {
-            if c == ' ' { continue }
-            else if let Some(d) = c.to_digit(10) { digits.push(d); }
-            else { return None }
-        };
-        Some(digits)
+                if d > 4 && i % 2 == 1 { total -= 9 }
+                i += 1;
+            } else if c != ' ' { return false }
+        }
+        i > 1 && total % 10 == 0
     }
 }
-impl Luhn for &'static str { fn digits(&self) -> Option<Vec<u32>> { self.to_string().digits() } }
-impl Luhn for u8 { fn digits(&self) -> Option<Vec<u32>> { self.to_string().digits() } }
-impl Luhn for u16 { fn digits(&self) -> Option<Vec<u32>> { self.to_string().digits() } }
-impl Luhn for u32 { fn digits(&self) -> Option<Vec<u32>> { self.to_string().digits() } }
-impl Luhn for u64 { fn digits(&self) -> Option<Vec<u32>> { self.to_string().digits() } }
-impl Luhn for usize { fn digits(&self) -> Option<Vec<u32>> { self.to_string().digits() } }
