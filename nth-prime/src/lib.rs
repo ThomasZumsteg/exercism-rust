f18f38823d2e7ebf3e13f2ee4a6fcba68c01ea68
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-10-04 22:10:01 -0700

    Faster version of nth-prime

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,17 +1,13 @@
 pub fn nth(n: isize) -> Result<isize, &'static str> {
     if n <= 0 { return Err("Needs to be greater than 0") }
-    let mut primes = Vec::new();
-    primes.push(2);
-    primes.push(3);
-    primes.push(5);
-    primes.push(7);
+    let mut primes = vec![2,3,5,7];
     let mut m = 9;
     while (primes.len() as isize) < n {
         let mut prime = true;
-        for p in &mut prime {
-            if *p * *p > m { 
+        for p in &primes {
+            if p * p > m { 
                 break;
-            } else if m % *p == 0 {
+            } else if m % p == 0 {
                 prime = false;
                 break;
             }
@@ -19,5 +15,5 @@ pub fn nth(n: isize) -> Result<isize, &'static str> {
         if prime { primes.push(m) }
         m += 2;
     }
-    Ok(primes[((n - 1) as usize)])
+    Ok(primes[(n - 1) as usize])
 }
