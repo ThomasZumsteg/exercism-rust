5b1e0127f3394aac40e97c989681a7f6a5a6d5ae
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-10-06 23:50:07 -0700

    Start on alphanumerics

---
+++ alphametics/src/lib.rs
@@ -0,0 +1,67 @@
+use std::collections::HashMap;
+
+pub fn solve(puzzle: &str) -> Option<HashMap<char, u8>> {
+    let equation = Equation::parse(puzzle).unwrap();
+    println!("{:?}", equation);
+    for e in equation { if e.evaluate() { return Some(e.get_map()) } }
+    None
+}
+
+#[derive(Debug, Clone)]
+struct Equation { 
+    left: Vec<String>,
+    right: String,
+    letters: Vec<(char, u8)>,
+    idx: usize,
+}
+
+impl Equation {
+    fn parse(source: &str) -> Option<Equation> {
+        let sides = source.split(" == ")
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>();
+        let right = sides[1].clone();
+        let left = sides[0].split(" + ")
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>();
+        Some( Equation{
+            left: left,
+            right: right,
+            letters: Equation::letters(source).iter().enumerate()
+                .map(|(i, &c)| (c, i as u8)).collect(),
+            idx: 0,
+        })
+    }   
+
+    fn get_map(&self) -> HashMap<char, u8> { 
+        let mut map = HashMap::new();
+        for &(c, i) in &self.letters { map.insert(c, i); }
+        map
+    }
+
+    fn letters(sentence: &str) -> Vec<char> {
+        let mut result = Vec::new();
+        for c in sentence.chars() {
+            if c.is_alphabetic() && !result.contains(&c) { result.push(c) }
+        }
+        result
+    }
+    fn evaluate(&self) -> bool{ 
+        false
+    }
+}
+
+impl Iterator for Equation {
+    type Item = Self;
+    fn next<'a>(&mut self) -> Option<Self::Item> {
+        let mut new = self.clone();
+        while new.letters[new.idx].1 >= 9 {
+            new.letters[new.idx].1 = 0;
+            new.idx += 1;
+            if new.idx > new.letters.len() {
+                return None
+            }
+        }
+        unimplemented!()
+    }
+}
