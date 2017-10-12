2d6ac5fc4273a3ceb57e3817ccbf0d6d6f08041e
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-10-11 21:25:40 -0700

    More work on poker

--- poker/src/lib.rs
+++ poker/src/lib.rs
@@ -7,6 +7,7 @@ use std::collections::HashMap;
 pub fn winning_hands<'a>(hands: &[&'a str]) -> Option<Vec<&'a str>> {
     let mut winners: Vec<&'a str> = Vec::new();
     for hand in hands {
+        println!("{:?}: {}", score(hand), hand);
         if winners.len() == 0 || 
             score(hand) == score(winners[0]) { winners.push(hand) }
         else if score(hand) > score(winners[0]) {
@@ -18,21 +19,54 @@ pub fn winning_hands<'a>(hands: &[&'a str]) -> Option<Vec<&'a str>> {
     Some(winners)
 }
 
-fn score(hand: &str) -> (usize, usize) {
+fn score(hand: &str) -> (usize, Vec<usize>) {
     let ranks = [high_card, one_pair, two_pair, three_of_a_kind, 
         straight, flush, full_house, four_of_a_kind, straigh_flush];
     for (r, func) in ranks.iter().enumerate().rev() {
         if let Some(score) = func(hand) { return (r, score) }
     }
-    return (0, 0)
+    return (0, Vec::new())
 }
 
-fn high_card(hand: &str) -> Option<usize> {None}
-fn one_pair(hand: &str) -> Option<usize> {None}
-fn two_pair(hand: &str) -> Option<usize> {None}
-fn three_of_a_kind(hand: &str) -> Option<usize> {None}
-fn straight(hand: &str) -> Option<usize> {None}
-fn flush(hand: &str) -> Option<usize> {None}
-fn full_house(hand: &str) -> Option<usize> {None}
-fn four_of_a_kind(hand: &str) -> Option<usize> {None}
-fn straigh_flush(hand: &str) -> Option<usize> {None}
+fn value(v: &str) -> usize {
+    "2 3 4 5 6 7 8 9 10 J Q K A".split(" ").position(|i| i == v).unwrap()
+}
+
+fn suit(s: &str) -> usize { "HSCD".split("").position(|i| i == s).unwrap() }
+
+fn suits_and_values(hand: &str) -> (HashMap<usize, usize>, HashMap<usize, usize>) {
+    let mut values: HashMap<usize, usize> = HashMap::new();
+    let mut suits: HashMap<usize, usize> = HashMap::new();
+    for card in hand.split(' ') {
+        *values.entry(value(&card[..card.len()-1])).or_insert(0) += 1;
+    }
+    (values, suits)
+}
+
+fn high_card(hand: &str) -> Option<Vec<usize>> {
+    let (values, _) = suits_and_values(hand);
+    if values.values().cloned().collect::<Vec<usize>>() == [1,1,1,1,1] { 
+        let mut result: Vec<usize> = values.keys().cloned().collect();
+        result.sort();
+        Some(result)
+    } else { None }
+}
+
+fn one_pair(hand: &str) -> Option<Vec<usize>> {
+    let mut value_map = suits_and_values(hand).0;
+    let mut value_list = value_map.values().cloned().collect::<Vec<usize>>(); 
+    value_list.sort();
+    if value_list == [1,1,1,2] {
+        let mut result = value_map.keys().cloned().collect::<Vec<usize>>();
+        result.sort();
+        Some(result)
+    } else {None}
+}
+
+fn two_pair(hand: &str) -> Option<Vec<usize>> {None}
+fn three_of_a_kind(hand: &str) -> Option<Vec<usize>> {None}
+fn straight(hand: &str) -> Option<Vec<usize>> {None}
+fn flush(hand: &str) -> Option<Vec<usize>> {None}
+fn full_house(hand: &str) -> Option<Vec<usize>> {None}
+fn four_of_a_kind(hand: &str) -> Option<Vec<usize>> {None}
+fn straigh_flush(hand: &str) -> Option<Vec<usize>> {None}
--- poker/tests/poker.rs
+++ poker/tests/poker.rs
@@ -38,7 +38,6 @@ fn test_highest_card_of_all_hands_wins() {
 }
 
 #[test]
-#[ignore]
 fn test_a_tie_has_multiple_winners() {
     test(
         &vec![
@@ -52,7 +51,6 @@ fn test_a_tie_has_multiple_winners() {
 }
 
 #[test]
-#[ignore]
 fn test_high_card_can_be_low_card_in_an_otherwise_tie() {
     // multiple hands with the same high cards, tie compares next highest ranked,
     // down to last card
@@ -63,7 +61,6 @@ fn test_high_card_can_be_low_card_in_an_otherwise_tie() {
 }
 
 #[test]
-#[ignore]
 fn test_one_pair_beats_high_card() {
     test(
         &vec!["4S 5H 6C 8D KH", "2S 4H 6S 4D JH"],
@@ -72,7 +69,6 @@ fn test_one_pair_beats_high_card() {
 }
 
 #[test]
-#[ignore]
 fn test_highest_pair_wins() {
     test(
         &vec!["4S 2H 6S 2D JH", "2S 4H 6C 4D JD"],
