702b1e50e090618207793d276a66bd42362f59d4
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-10-16 19:59:41 -0700

    Robot-name now with uniqueness guaranteed

--- robot-name/src/lib.rs
+++ robot-name/src/lib.rs
@@ -1,6 +1,11 @@
 extern crate rand;
+use std::collections::HashSet;
+use std::cell::RefCell;
 
 pub struct Robot { name: String }
+
+thread_local!(static NAMES_IN_USE:RefCell<HashSet<String>> = 
+              RefCell::new(HashSet::new()));
 
 impl Robot {
     pub fn new() -> Robot { Robot { name: Self::get_unique_name() } }
@@ -11,9 +16,17 @@ impl Robot {
         let letters: Vec<char> = (0..26).map(|n| (n + 'A' as u8) as char).collect();
         let digits: Vec<char> = (0..10).map(|n| (n + '0' as u8) as char).collect();
         let mut rng = rand::thread_rng();
-        vec![&letters, &letters, &digits, &digits, &digits]
-            .iter()
-            .map(|set| rand::sample(&mut rng, set.iter(), 1)[0])
-            .collect()
+        let mut name = String::new();
+        NAMES_IN_USE.with(|cell| {
+            let mut names = cell.borrow_mut();
+            while name == "" || names.contains(&name) {
+                name = vec![&letters, &letters, &digits, &digits, &digits]
+                    .iter()
+                    .map(|set| rand::sample(&mut rng, set.iter(), 1)[0])
+                    .collect();
+            }
+            names.insert(name.clone());
+            name
+        })
     }
 }
