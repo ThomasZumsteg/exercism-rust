a19577121db7575afce24374d683dd3d63f002e7
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-10-23 21:39:58 -0700

    More work on decimal

--- decimal/src/lib.rs
+++ decimal/src/lib.rs
@@ -1,5 +1,6 @@
 use std::ops::{Mul, Add, Sub};
-use std::cmp::Ordering;
+use std::cmp::{Ordering, max};
+use std::iter;
 
 type Digit = u8;
 const Base: usize = 10;
@@ -13,31 +14,45 @@ pub struct Decimal {
 
 impl Decimal {
     pub fn try_from(chars: &str) -> Option<Decimal> {
-        let mut digits: Vec<Digit> = Vec::new();
-        let mut power: usize = 0;
-        for (i, digit) in chars.chars().rev().enumerate() {
-            if digit == '.' { power = i }
+        let mut number = Decimal { digits: Vec::new(), negative: false, power: 0};
+        let mut start = 0;
+        if chars.starts_with("-") {
+            start = 1;
+            number.negative = true;
+        }
+        for (i, digit) in chars[start..].chars().rev().enumerate() {
+            if digit == '.' { number.power = i }
             else if let Some(d) = digit.to_digit(Base as u32) { 
-                digits.insert(0, d as Digit) 
+                number.digits.insert(0, d as Digit) 
             } else { return None }
         };
-        Some(Decimal { negative: false, power: power, digits: digits })
+        Some(number)
     }
 
-    fn digits(&self, power: usize) -> Option<Vec<Digit>> { None }
+    fn zip_digits(&self, other: &Decimal) -> Vec<(Digit, Digit)> { 
+        if self.power > other.power { other.zip_digits(&self) }
+        else {
+            let other_digits = other.digits.iter().cloned().chain(iter::repeat(0));
+            self.digits.iter().cloned().zip(other_digits).collect()
+        }
+    }
 }
 
 
 impl Add for Decimal {
     type Output = Decimal;
     fn add(self, other: Decimal) -> Decimal { 
-        let mut digits: Vec<Digit> = Vec::new();
+        let mut result = Decimal { 
+            digits: Vec::new(),
+            power: max(self.power, other.power),
+            negative: false
+        };
         let mut carry = 0;
         for (&a, &b) in self.digits.iter().zip(other.digits.iter()).rev() {
-            digits.push((a + b + carry) % (Base as u8));
+            result.digits.push((a + b + carry) % (Base as u8));
             let carry = (a + b + carry) / (Base as u8);
         }
-        Decimal{ negative: false, digits: digits, power: self.power }
+        result
     }
 }
 
@@ -53,13 +68,8 @@ impl Mul for Decimal {
 
 impl PartialOrd for Decimal {
     fn partial_cmp(&self, other: &Decimal) -> Option<Ordering> { 
-        if self.power != other.power { 
-            return self.power.partial_cmp(&other.power)
-        } else if self.digits.len() != other.digits.len() {
-            return self.digits.len().partial_cmp(&other.digits.len())
-        }
-        for (a, b) in self.digits.iter().zip(other.digits.iter()) {
-            if a != b { return a.partial_cmp(b) }
+        for (a, b) in self.zip_digits(other) {
+            if a != b { return a.partial_cmp(&b) }
         }
         Some(Ordering::Equal)
     }
