054c69fbbf9c1d8c622bc4e54da1f5d5d6b15fef
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-11-13 21:35:45 -0800

    Work on thread pool

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -1,14 +1,16 @@
 use std::collections::HashMap;
 use std::thread;
 use std::sync::mpsc;
+use std::sync::Arc;
 
-struct ThreadPool {
+struct ThreadPool<S,T> {
+    jobs: mpsc::Sender<S>,
+    results: mpsc::Receiver<T>,
     threads: Vec<thread::JoinHandle<()>>,
 }
 
 pub fn frequency(lines: &[&str], _: usize) -> HashMap<char, usize> {
     let mut result = HashMap::new();
-    let (tx, rx) = mpsc::channel::<&str>();
     for line in lines {
         for c in line.to_lowercase().chars() {
             if c.is_alphabetic() { *result.entry(c).or_insert(0) += 1; }
@@ -17,13 +19,22 @@ pub fn frequency(lines: &[&str], _: usize) -> HashMap<char, usize> {
     result
 }
 
-impl ThreadPool {
-    fn new<F>(f: F, n_workers: usize) -> ThreadPool 
+impl <S,T> ThreadPool<S,T> 
+    where S: Send {
+    fn new<F>(f: F, n_workers: usize) -> ThreadPool<S,T>
         where 
             F: FnOnce() + Send + 'static {
-        let mut pool = ThreadPool{ threads: Vec::with_capacity(n_workers) };
+        let (job_tx, job_rx) = mpsc::channel::<S>();
+        let (result_tx, result_rx) = mpsc::channel::<T>();
+        let mut pool: ThreadPool<S,T> = ThreadPool { 
+            jobs: job_tx,
+            results: result_rx,
+            threads: Vec::with_capacity(n_workers),
+        };
+        let thread_rx = Arc::new(job_rx);
         for _ in 0..n_workers {
-            pool.threads.push(thread::spawn(|| {}));
+            pool.threads.push(thread::spawn(|| {
+            }));
         }
         pool
     }
