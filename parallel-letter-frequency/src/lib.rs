e07e08478be05e54b19a3888e31153a6f45d1b36
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-11-18 21:13:32 -0800

    More work on parallel letter frequency

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -1,16 +1,24 @@
 use std::collections::HashMap;
 use std::collections::hash_map::Entry;
 use std::thread;
+use std::fmt::Debug;
 use std::sync::{Arc, Mutex, mpsc};
 
 pub fn frequency(lines: &[&str], n_workers: usize) -> HashMap<char, usize> {
     let mut result = HashMap::new();
+    println!("Creating pool");
     let pool = ThreadPool::new(n_workers, frequency_job);
-    for job in pool.do_jobs(lines.to_vec()) { result.merge(job); }
+    println!("Done");
+    let jobs = lines.iter().map(|s| s.to_string());
+    println!("Starting jobs");
+    for job in pool.do_jobs(jobs) { 
+        println!("Merging {:?}", job);
+        result.merge(job);
+    }
     result
 }
 
-fn frequency_job(line: &str) -> HashMap<char, usize> {
+fn frequency_job(line: String) -> HashMap<char, usize> {
     let mut result = HashMap::new();
     for c in line.to_lowercase().chars() {
         if c.is_alphabetic() { *result.entry(c).or_insert(0) += 1 }
@@ -35,46 +43,57 @@ impl <K,V> Merge<HashMap<K,V>> for HashMap<K,V>
     }
 }
 
-struct ThreadPool<S,T> {
+struct ThreadPool<S,T> 
+{
     f: fn(S) -> T,
-    threads: Vec<thread::JoinHandle<usize>>,
-}
-
-struct ThreadPoolIterator<T> {
+    threads: Vec<thread::JoinHandle<()>>,
+    queue: Vec<mpsc::Sender<S>>,
     rx: mpsc::Receiver<T>,
 }
 
-impl <S,T> IntoIterator for ThreadPool<S,T> {
-    type Item = T;
-    type IntoIter = ThreadPoolIterator<T>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        unimplemented!()
-    }
-}
-
-impl <S,T> ThreadPool<S,T> {
+impl <'a,S,T> ThreadPool<S,T>
+    where
+        S: Debug + Send + 'static,
+        T: Debug + Send + 'static,
+{
     fn new(n_workers: usize, f: fn(S) -> T) -> ThreadPool<S,T> {
-        let mut pool = ThreadPool { f: f, threads: Vec::new() };
+        let (results_tx, results_rx) = mpsc::channel::<T>();
+        let mut pool = ThreadPool { 
+            f: f,
+            threads: Vec::new(),
+            queue: Vec::new(),
+            rx: results_rx,
+        };
+        for i in 0..n_workers {
+            let (jobs_tx, jobs_rx) = mpsc::channel::<S>();
+            let thread_results = results_tx.clone();
+            pool.threads.push(thread::spawn(move || {
+                while let Ok(item) = jobs_rx.recv() {
+                    println!("Thread {}: Doing work", i);
+                    thread_results.send(f(item));
+                }
+            }));
+            pool.queue.push(jobs_tx);
+        }
         pool
     }
 
-    fn do_jobs<I>(&self, jobs: I) -> ThreadPoolIterator<T>
+    fn do_jobs<I>(&self, jobs: I) -> &Self
         where 
             I: IntoIterator<Item=S> {
+        println!("Doing jobs");
         let (tx, rx) = mpsc::channel();
         for job in jobs {
             tx.send((self.f)(job)); 
         }
-        ThreadPoolIterator { rx: rx }
+        self
     }
 }
 
-impl <T> Iterator for ThreadPoolIterator<T> {
+impl <S,T> Iterator for ThreadPool<S,T> {
     type Item = T;
-
     fn next(&mut self) -> Option<T> {
-        if let Ok(result) = self.rx.recv() { Some(result) }
-        else { None }
+        unimplemented!()
     }
 }
+
