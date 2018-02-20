df02dd65652e8ec9ca51a2919e6b44322bba60c7
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2018-02-19 21:16:17 -0800

    More work on react

--- react/src/lib.rs
+++ react/src/lib.rs
@@ -6,9 +6,17 @@ pub type CellID = usize;
 pub type CallbackID = usize;
 
 struct Cell<T> {
-    value: T,
-    update: Option<Box<Fn() -> Result<T, ()>>>,
+    value: T, 
+    dependencies: Vec<Cell<T>>,
+    update: Option<Box<Fn(&[T]) -> T>>,
     callbacks: Vec<Box<Fn(T)>>,
+}
+
+impl <T: Copy + PartialEq> Cell<T> {
+    pub fn set_value(&mut self, new_value: T) {
+        if new_value == self.value { return }
+        self.value = new_value;
+    }
 }
 
 pub struct Reactor<T> {
@@ -23,99 +31,59 @@ impl <T: Copy + PartialEq> Reactor<T> {
 
     // Creates an input cell with the specified initial value, returning its ID.
     pub fn create_input(&mut self, initial: T) -> CellID {
-        self.cells.push(Cell { 
+        self.cells.push(Cell {
             value: initial,
-            update: None,
             callbacks: Vec::new(),
+            update: None,
+            dependencies: Vec::new(),
         });
         self.cells.len() - 1
     }
 
-    // Creates a compute cell with the specified dependencies and compute function.
-    // The compute function is expected to take in its arguments in the same order as specified in
-    // `dependencies`.
-    // You do not need to reject compute functions that expect more arguments than there are
-    // dependencies (how would you check for this, anyway?).
-    //
-    // Return an Err (and you can change the error type) if any dependency doesn't exist.
-    //
-    // Notice that there is no way to *remove* a cell.
-    // This means that you may assume, without checking, that if the dependencies exist at creation
-    // time they will continue to exist as long as the Reactor exists.
     pub fn create_compute<F: Fn(&[T]) -> T + 'static>(&mut self, dependencies: &[CellID], compute_func: F) -> Result<CellID, ()> {
-        let update_func = Box::new(move || {
-            let mut values = Vec::new();
-            for &d in dependencies {
-                if let Some(v) = self.value(d) {
-                    values.push(v);
-                } else {
-                    return Err(())
-                }
-            }
-            Ok(compute_func(&values))
+        let cells: Vec<Cell<T>> = dependencies.iter().map(|d| 
+            match self.cells.get(d) {
+                Some(c) => c,
+                None => return Err(()),
+            }).collect();
+        let values = cells.iter().map(|c| c.value).collect();
+        self.cells.push(Cell{
+            value: compute_func(values),
+            update: Some(Box::new(compute_func)),
+            dependencies: cells,
+            callbacks: Vec::new()
         });
-        if let Ok(inital) = update_func() {
-            self.cells.push(Cell {
-                update: Some(update_func),
-                value: inital,
-                callbacks: Vec::new(),
-            });
-        }
         Ok(self.cells.len() - 1)
     }
 
-    // Retrieves the current value of the cell, or None if the cell does not exist.
-    //
-    // You may wonder whether it is possible to implement `get(&self, id: CellID) -> Option<&Cell>`
-    // and have a `value(&self)` method on `Cell`.
-    //
-    // It turns out this introduces a significant amount of extra complexity to this exercise.
-    // We chose not to cover this here, since this exercise is probably enough work as-is.
     pub fn value(&self, id: CellID) -> Option<T> {
         self.cells.get(id).and_then(|c| Some(c.value))
     }
 
-    // Sets the value of the specified input cell.
-    //
-    // Return an Err (and you can change the error type) if the cell does not exist, or the
-    // specified cell is a compute cell, since compute cells cannot have their values directly set.
-    //
-    // Similarly, you may wonder about `get_mut(&mut self, id: CellID) -> Option<&mut Cell>`, with
-    // a `set_value(&mut self, new_value: T)` method on `Cell`.
-    //
-    // As before, that turned out to add too much extra complexity.
     pub fn set_value(&mut self, id: CellID, new_value: T) -> Result<(), ()> {
+        let mut result = Err(());
         if let Some(cell) = self.cells.get_mut(id) {
-            cell.value = new_value;
-            Ok(())
-        } else {
-            Err(())
+            if cell.update.is_none() {
+                cell.set_value(new_value);
+                result = Ok(());
+            }
         }
+        if result.is_ok() { self.update(); }
+        result
     }
 
-    // Adds a callback to the specified compute cell.
-    //
-    // Return an Err (and you can change the error type) if the cell does not exist.
-    //
-    // Callbacks on input cells will not be tested.
-    //
-    // The semantics of callbacks (as will be tested):
-    // For a single set_value call, each compute cell's callbacks should each be called:
-    // * Zero times if the compute cell's value did not change as a result of the set_value call.
-    // * Exactly once if the compute cell's value changed as a result of the set_value call.
-    //   The value passed to the callback should be the final value of the compute cell after the
-    //   set_value call.
     pub fn add_callback<F: FnMut(T) -> ()>(&mut self, id: CellID, callback: F) -> Result<CallbackID, ()> {
         unimplemented!()
     }
 
-    // Removes the specified callback, using an ID returned from add_callback.
-    //
-    // Return an Err (and you can change the error type) if either the cell or callback
-    // does not exist.
-    //
-    // A removed callback should no longer be called.
     pub fn remove_callback(&mut self, cell: CellID, callback: CallbackID) -> Result<(), ()> {
         unimplemented!()
+    }
+
+    fn update(&mut self) {
+        for cell in &mut self.cells.iter_mut() {
+            if cell.update.is_none() { continue; }
+            unimplemented!()
+        }
     }
 }
