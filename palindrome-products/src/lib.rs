c3c4a2402e1871ce05032d821bd077f97aa398b5
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2018-09-26 22:51:24 +0000

    With threads

--- palindrome-products/src/lib.rs
+++ palindrome-products/src/lib.rs
@@ -1,16 +1,40 @@
+use std::thread;
+use std::sync::mpsc;
 use std::collections::HashSet;
+
+static NTHREADS: u64 = 3;
 
 pub type Palindrome = u64;
 pub fn get_palindrome_products(min: u64, max: u64) -> Vec<Palindrome> {
-    let mut products = HashSet::new();
-    for i in min..(max+1) {
-        for j in i..(max+1) {
-            let product = i * j;
-            let digits = format!("{}", product);
-            if digits.chars().into_iter().eq(digits.chars().into_iter().rev()) {
-                products.insert(product);
+    let mut pool = Vec::new();
+    let (tx, rx) = mpsc::channel::<Palindrome>();
+    for t in 0..NTHREADS {
+        let thread_tx = tx.clone();
+        pool.push(Some(thread::spawn(move || {
+            let mut i = min + t;
+            while i <= max {
+                for j in i..max+1 {
+                    let product = i * j;
+                    let digits = format!("{}", product);
+                    if digits.chars().into_iter().eq(digits.chars().into_iter().rev()) {
+                        thread_tx.send(product).unwrap();
+                    } 
+                }
+                i += NTHREADS;
             }
+        })));
+    }
+    
+    for handle in &mut pool {
+        if let Some(thread) = handle.take() {
+            thread.join().unwrap();
         }
+    }
+    drop(tx);
+    
+    let mut products = HashSet::new();
+    for p in rx {
+        products.insert(p);
     }
     products.iter().map(|&p| p).collect()
 }
